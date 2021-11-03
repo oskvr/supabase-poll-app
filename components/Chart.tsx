5f9af9fd280b3d5a0a51aed0aacb7a0734471d68
Lucas <lucasersson.no@gmail.com>
2021-11-03 12:18:29 +0100

    added option

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -1,31 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { Bar } from "react-chartjs-2";
-import { useRandomColors } from "../hooks";
-import { Poll, PollOption } from "../lib/models/poll";
+import React, { useEffect, useState } from 'react'
+import { Bar } from 'react-chartjs-2'
+import { useRandomColors, useStandardColors } from '../hooks'
+import { Poll, PollOption } from '../lib/models/poll'
 
 interface PollChartProps {
-  poll: Poll;
+  poll: Poll
 }
 export default function Chart({ poll }: PollChartProps) {
-  const colors = useRandomColors(poll.options?.length);
+  const colors = useRandomColors(poll.options?.length)
+  // const colors = useStandardColors(poll.options?.length)
   const pollOptionLabels = poll.options?.map((option: PollOption) => {
-    return option.description;
-  });
-  const voteCountData = poll.options?.map((option) => {
-    return option.votes.length;
-  });
+    return option.description
+  })
+  const voteCountData = poll.options?.map(option => {
+    return option.votes.length
+  })
 
   const data = {
     labels: pollOptionLabels,
     datasets: [
       {
-        label: "# of votes",
+        label: '# of votes',
         data: voteCountData,
         backgroundColor: colors,
         borderWidth: 1,
       },
     ],
-  };
-  const options = { aspectRatio: 2 };
-  return <Bar data={data} options={options} />;
+  }
+  const options = { aspectRatio: 2 }
+  return <Bar data={data} options={options} />
 }
